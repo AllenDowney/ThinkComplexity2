946a2b0e46979a5f2e96b9f5ad23c8e49cd06f73
Allen Downey <downey@allendowney.com>
2016-07-25 16:24:18 -0400

    Updating Life.py

--- code/Life.py
+++ code/Life.py
@@ -6,17 +6,26 @@ http://greenteapress.com/complexity
 Copyright 2016 Allen Downey
 MIT License: http://opensource.org/licenses/MIT
 """
+from __future__ import print_function, division
 
 import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
+from matplotlib import animation
 from scipy.signal import convolve2d
 
+"""
+For animation to work in the notebook, you might have to install
+ffmpeg.  On Ubuntu and Linux Mint, the following should work.
 
-class Life(object):
+    sudo add-apt-repository ppa:mc3man/trusty-media
+    sudo apt-get update
+    sudo apt-get install ffmpeg
+"""
+
+class Life:
     """Implements Conway's Game of Life.
 
     n:     the number of rows and columns
@@ -28,22 +37,23 @@ class Life(object):
         """Attributes:
         n:      number of rows and columns
         """
+        m = n if m is None else m
         self.n = n
-        self.m = n if m is None else m
+        self.m = m
         self.array = np.zeros((n, m), np.uint8)
         self.kernel = np.array([[1, 1, 1],
-                                   [1,10, 1],
-                                   [1, 1, 1]])
+                                [1,10, 1],
+                                [1, 1, 1]])
 
-    def add_cells(self, x, y, *strings):
+    def add_cells(self, row, col, *strings):
         """Adds cells at the given location.
         
-        x: left coordinate
-        y: top coordinate
+        row: top row index
+        col: left col index
         strings: list of strings of 0s and 1s
         """
         for i, s in enumerate(strings):
-            self.array[y+i, x:x+len(s)] = np.array([int(b) for b in s])
+            self.array[row+i, col:col+len(s)] = np.array([int(b) for b in s])
 
     def step(self):
         """Executes one time step."""
@@ -57,22 +67,31 @@ class LifeViewer:
         self.life = life
         self.im = None
     
-    def draw(self):
+    def draw(self, grid=False):
         """Updates the display with the state of the grid."""
         a = self.life.array
         cmap = plt.get_cmap('Greens')
         self.im = plt.imshow(a, cmap=cmap, interpolation='none')
+        if grid:
+            n, m = self.life.n, self.life.m
+            lw = 2 if m < 10 else 1
+            rows = np.arange(n-1) + 0.5 - lw/200 
+            plt.hlines(rows, -0.5, m-0.5, color='white', linewidth=lw)
+
+            cols = np.arange(m-1) + 0.5 - lw/200
+            plt.vlines(cols, -0.5, n-0.5, color='white', linewidth=lw)
+
         plt.xticks([])
         plt.yticks([])
 
-    def animate(self, frames=20, interval=200):
+    def animate(self, frames=20, interval=200, grid=False):
         """Creates an animation.
         
         frames: number of frames to draw
         interval: time between frames in ms
         """
         fig = plt.figure()
-        self.draw()
+        self.draw(grid)
         anim = animation.FuncAnimation(fig, self.animate_func, 
                                        frames=frames, interval=interval)
         return anim
@@ -114,7 +133,7 @@ bhep = [
 
 def main(script, *args):
     n = 200
-    m = 1200
+    m = 1000
     life = Life(n, m)
     #life.add_cells(n//2, n//2, '11101', '1', '00011', '01101', '10101')
     #life.add_cells(n//2, n//2, *glider_gun)
@@ -123,8 +142,8 @@ def main(script, *args):
     life.add_cells(x, n//2+6, *lwss)
     life.add_cells(x, n//2-1, *bhep)
     viewer = LifeViewer(life)
-    #viewer.draw()
     anim = viewer.animate(frames=100, interval=0)
+    plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99)
     plt.show()
 
 if __name__ == '__main__':
