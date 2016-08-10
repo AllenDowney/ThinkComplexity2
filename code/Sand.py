dc28ea23a85df23461cf7dbe9fe08798dd71d666
Allen Downey <downey@allendowney.com>
2016-08-10 12:50:00 -0400

    Adding sspile.ipynb

--- code/Sand.py
+++ code/Sand.py
@@ -14,21 +14,33 @@ class SandPile(Cell2D):
                        [1,-4, 1],
                        [0, 1, 0]], dtype=np.int32)
 
-    def __init__(self, n, m=None):
+    def __init__(self, n, m=None, level=9):
         """Initializes the attributes.
 
         n: number of rows
         m: number of columns
-        r: diffusion rate constant
+        level: starting value for all cells
         """
         m = n if m is None else m
-        choices = np.array([2, 3, 4], dtype=np.int32)
-        self.array = np.random.choice(choices, (n, m))
-        self.total = np.zeros((n, m), dtype=np.int32)
-        self.swept_seq = []
+        self.array = np.ones((n, m)) * level
+        self.reset()
+
+    def reset(self):
+        """Start keeping track of the number of toppled cells.
+        """
         self.toppled_seq = []
 
-    def step(self, K=4):
+    def single_source(pile, height=1024):
+        """Adds a tower to the center cell.
+    
+        height: value assigned to the center cell
+        """
+        a = pile.array
+        n, m = a.shape
+        a[:, :] = 0
+        a[n//2, m//2] = height
+
+    def step(self, K=3):
         """Executes one time step.
         
         returns: number of cells that toppled
@@ -37,23 +49,12 @@ class SandPile(Cell2D):
         n, m = a.shape
         
         toppling = a>K
-        self.total += toppling
-        
         num_toppled = np.sum(toppling)
+        self.toppled_seq.append(num_toppled)
+
         c = correlate2d(toppling, self.kernel, mode='same')
         self.array += c
         return num_toppled
-    
-    def sweep(self):
-        """Sweeps the edges.
-        
-        returns: total of all swept cells
-        """
-        a = self.array
-        total = np.sum(a[[0, -1]]) + np.sum(a[:, [0, -1]])
-        a[[0, -1]] = 0
-        a[:, [0, -1]] = 0
-        return total
     
     def drop(self):
         """Increments a random cell."""
@@ -69,61 +70,47 @@ class SandPile(Cell2D):
         """
         total = 0
         for i in itertools.count(1):
-            swept = self.sweep()
-            toppled = self.step()
-
-            self.swept_seq.append(swept)
-            self.toppled_seq.append(toppled)
-            
-            total += toppled
-            if toppled == 0:
+            num_toppled = self.step()
+            total += num_toppled
+            if num_toppled == 0:
                 return i, total
-            
+
     def drop_and_run(self):
         """Drops a random grain and runs to equilibrium.
         
-        returns: duration, swept, toppled
+        returns: duration, total_toppled
         """
         self.drop()
-        duration, toppled = self.run()
-        return duration, toppled
+        duration, total_toppled = self.run()
+        return duration, total_toppled
 
 
 class SandPileViewer(Cell2DViewer):
     cmap = plt.get_cmap('YlOrRd')
     options = dict(interpolation='none', alpha=0.8,
-                   vmin=0, vmax=7)
+                   vmin=0, vmax=5)
     
-#    def step(self, iters=1):
-#        """Advances the viewee the given number of steps."""
-#        for i in range(iters):
-#            self.viewee.drop_and_run()
-            
-    def draw_total(self, mod_flag=False):
-        cmap = plt.get_cmap('YlOrRd')
-        options = dict(interpolation='none', alpha=0.8)
-        if mod_flag:
-            a = self.viewee.total % 2
+    def __init__(self, viewee, drop_flag=True):
+        """Initializes the attributes.
+
+        drop_flag: determines whether `step` drops a grain
+        """
+        Cell2DViewer.__init__(self, viewee)
+        self.drop_flag = drop_flag
+
+    def step(self):
+        """Advances the viewee one step."""
+        if self.drop_flag:
+            self.viewee.drop_and_run()
         else:
-            a = self.viewee.total
-            
-        self.draw_array(a, cmap, **options)
-
-
-def tower(pile, factor=2):
-    a = pile.array
-    n, m = a.shape
-    
-    a[:, :] = 0
-    a[n//2, m//2] = factor * n * m
-
+            self.viewee.step()
 
 
 def main():
     n = 100
     pile = SandPile(n)
     tower(pile)
-    viewer = SandPileViewer(pile)
+    viewer = SandPileViewer(pile, drop_flag=False)
     anim = viewer.animate(interval=0)
     plt.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99)
     plt.show()
