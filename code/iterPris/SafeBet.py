e8bf456d1d1499978c7b430789ee51e0a445c085
willythor <willem.thorbecke@students.olin.edu>
2016-10-31 16:06:39 -0400

    Delete SafeBet.py

--- code/iterPris/SafeBet.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Trustworthy():
-    ''' Will hopefully pose as trustworthy to other prisoners
-    '''
-    def step(self, history, round):
-	    if round%5 == 4:
-	    	return 0
-	    else: return 1 
